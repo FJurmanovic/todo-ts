2444bf32d6aea460bf24b1d28b0f9fd7ea6b5ad7
Fran Jurmanović <fjurma12@outlook.com>
2020-06-14 16:17:08 +0200

    More styling + thorn css framework

--- src/components/add.ts
+++ src/components/add.ts
@@ -5,9 +5,11 @@ const Add = () => {
     addDiv.className = 'add'
     let addInput = document.createElement("input");
     addInput.setAttribute("type", "text");
+    addInput.setAttribute("placeholder", "Add new todo")
     let addButton = document.createElement("button");
-    addButton.innerHTML = "Add"
+    addButton.innerHTML = "<div class='gg-math-plus'></div>"
     addButton.addEventListener("click", newItem)
+    addButton.className = "btn btn-icon-round"
     addDiv.appendChild(addInput)
     addDiv.appendChild(addButton)
 
